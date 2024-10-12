8b9099ac93db429eae1b3a8d7873121a5b6772d7
tudor <7089284+tudddorrr@users.noreply.github.com>
2024-10-12 16:52:30 +0100

    use stats.trytalo.com for plausible

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -137,7 +137,7 @@ module.exports = async function configCreatorAsync() {
     },
     scripts: [
       {
-        src: 'https://stats.sleepystudios.net/js/script.js',
+        src: 'https://stats.trytalo.com/js/script.js',
         defer: true,
         'data-domain': 'docs.trytalo.com'
       }
